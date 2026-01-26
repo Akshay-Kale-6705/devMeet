950d114a1b074d653c21e68ae3a179c1ba4e335a
Akshay-Kale-6705 <akshaykale6705@gmail.com>
2026-01-26 16:55:13 +0530

    profile edit

--- src/app.js
+++ src/app.js
@@ -2,85 +2,26 @@ const express = require("express");
 const connectDB = require("./config/database");
 const user = require("./models/user");
 const app = express();
-const User = require("./models/user");
-const { validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
+
+
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("./middleware/auth");
+
+
 
 //middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signup', async (req, res) => {
-    try {
-        //validation of data
-        validateSignUpData(req);
+//routes 
+const authRouter = require("./routes/auth");
+const profile = require("./routes/profile");
+const requests = require("./routes/request");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-        const { firstName, lastName, emailId, password } = req.body;
-
-        //Encrypt the password
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        //creating a new instance of the user model
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash,
-        });
-
-        await user.save();
-        res.send("User added sucessfully...")
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.post("/login", async (req, res) => {
-    try {
-        const { emailId, password } = req.body;
-
-        const user = await User.findOne({ emailId: emailId });
-        if (!user) {
-            throw new Error("Invalid credenatials");
-        }
-        const isPasswordValid = await user. validatePassword(password);
-        
-        if (isPasswordValid) {
-            //get jwt token inside an user.js
-            const token = await user.getJWT();
-            //Add the token to create and send the response back to the user
-            res.cookie("token", token, {
-                expires: new Date(Date.now() + 8 * 3600000), 
-            });
-            res.send("User login sucessful");
-        } else {
-            throw new Error("Invalid credenatials");
-        }
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-    try {
-       const user = req.user;
-
-        res.send(user);
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-    const user = req.user;
-    //sending a connection request
-    console.log("Sending connection request !");
-
-    res.send(user.firstName + " sent the connection request !");
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB()
     .then(() => {
